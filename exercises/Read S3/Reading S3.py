a9242bf243f50f0469dad33d4bcf07d02fd0f253
alfa01-github <amfarnan@gmail.com>
2022-12-09 13:34:13 +0000

    Assignment

--- exercises/Read S3/Reading S3.py
+++ exercises/Read S3/Reading S3.py
@@ -1,8 +1,13 @@
+#SECTION ONE OF THE ASSIGNMENT
+import os
+print(os.environ["AWS_ACCESS_KEY_ID"])
+
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
 BUCKET = "dmacademy-course-assets"
-KEY = "airflow/data.csv"
+KEYafter = "vlerick/after_release.csv"
+KEYpre = "vlerick/pre_release.csv"
 
 config = {
     "spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1",
@@ -11,5 +16,15 @@ config = {
 conf = SparkConf().setAll(config.items())
 spark = SparkSession.builder.config(conf=conf).getOrCreate()
 
-df = spark.read.csv(f"s3a://{BUCKET}/{KEY}", header=True)
-df.show()
+dfpre = spark.read.csv(f"s3a://{BUCKET}/{KEYpre}", header=True)
+dfpre.show()
+dfafter = spark.read.csv(f"s3a://{BUCKET}/{KEYafter}", header=True)
+dfafter.show()
+
+#SECTION TWO OF THE ASSIGNMENT
+
+import pandas as pd 
+
+pre = dfpre.toPandas()
+after = dfafter.toPandas()
+
