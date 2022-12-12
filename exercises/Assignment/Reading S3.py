8da4f6f74a6ead8c61fde7ef583f9a0e528d160c
alfa01-github <amfarnan@gmail.com>
2022-12-12 17:53:29 +0000

    Update

--- exercises/Assignment/Reading S3.py
+++ exercises/Assignment/Reading S3.py
@@ -1,9 +1,7 @@
-# PREREQUISITES FOR DOCKER?
-RUN python -m pip install -r requirementsfile.txt
-
 # SECTION ONE OF THE ASSIGNMENT
 import os
 print(os.environ["AWS_ACCESS_KEY_ID"])
+
 
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
@@ -14,8 +12,9 @@ KEYpre = "vlerick/pre_release.csv"
 
 config = {
     "spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1",
-    "spark.hadoop.fs.s3a.aws.credentials.provider": "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider",
+    "spark.hadoop.fs.s3a.aws.credentials.provider": "org.apache.hadoop.fs.s3a.InstanceProfileCredentialsProvider",
 }
+
 conf = SparkConf().setAll(config.items())
 spark = SparkSession.builder.config(conf=conf).getOrCreate()
 
@@ -31,6 +30,11 @@ import pandas as pd
 pre = dfpre.toPandas()
 after = dfafter.toPandas()
 
-# SECTION FIVE OF THE ASSIGNMENT
+# SECTION THREE OF THE ASSIGNMENT
+
+# SECTION FOUR OF THE ASSIGNMENT
+df = spark.createDataFrame(OUTPUT)
+
+
 
 
